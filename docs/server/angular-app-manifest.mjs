b5be6dabbd3e49fe4cd0ecc80ca95410610780dd
K-R-MIKHIL <krmikhiledu@gmail.com>
2025-01-06 02:32:53 +0530

    Deploy Angular production build to GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -11,9 +11,9 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 524, hash: 'a7782be2bf6ac73c40498d980e328e62aadf9d95d610aa20af0f77d17cde6591', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1037, hash: '47e30950736d86f962893188dc33b7dfd408b167436b4ef3fc73b576dc86f220', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 14926, hash: '8287bad2acc97bea1598ba2f1318fd0c52dda3e914e7485c91fcceb5c354eae8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 524, hash: '8d8224f28e6937403660e709dc8fb3b1b9eea424540dce01b2ab1269a343a293', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1037, hash: '2bfe9d91cd76e60c7f1c0d07907078cff489152d7ca7fc75cddf4ae1c1872732', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 14849, hash: '412991415f6b0c7b215ed23fc0d25783534c1da81950eee2ce49417c0d55abfb', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
